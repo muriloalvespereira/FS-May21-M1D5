951a437c36762542109d421eb77dd4cb978cfe8f
Murilo Alves <murilo.udi@gmail.com>
2021-05-23 14:20:59 +0200

    Completed exercise 3 weekend

--- weekendexercises.js
+++ weekendexercises.js
@@ -80,7 +80,10 @@ console.log(whoIsBigger(10,20))
     Write a function called "splitMe" which receives a string as a parameter and returns an array with every word in that string.
     Ex. splitMe("I love coding") => returns ["I", "Love", "Coding"]
 */
-
+function splitMe (str){
+    return str.split(" ")
+}
+console.log(splitMe("I Love Coding"))
 /* Ex.4
     Write a function called "deleteOne" which receives a string and a boolean as parameters. If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
 */
